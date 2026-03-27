ddf1129ebf083dbf5b619e059d2c758ba8fd290d
Prapangkorn Thangsathityangkul <XxtheNerdLordxX@outlook.com>
2026-03-27 15:01:22 +0700

    Initial commit: Spring Boot 3 Trip & Media Service scaffold

---
+++ src/main/java/com/navio/tripmediaservice/TripMediaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.navio.tripmediaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripMediaServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TripMediaServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/navio/tripmediaservice/TripMediaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.navio.tripmediaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripMediaServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
